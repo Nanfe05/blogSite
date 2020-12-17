d9fc10e50527a727cab9e9f40cc7785ebd6bc49c
juanfelipemartinezfernandez <65261935+juanfelipemartinezfernandez@users.noreply.github.com>
2020-12-16 21:51:24 -0500

    Update forms.py

--- application/forms.py
+++ application/forms.py
@@ -16,25 +16,30 @@ class LoginForm(Form):
         Length(min=6,max=12),
         ])
 
-
-# # FORM REGISTER
+#REGISTER
 class RegisterForm(Form):
-    name = StringField('name',[
+    name = StringField('name',validators=[
         DataRequired(message='Name is required'),
         Length(min=2,max=20)
         ])
-    lastname = StringField('lastname',[
+    lastname = StringField('lastname',validators=[
         DataRequired(message='Lastname is required'),
         Length(min=2,max=20)
         ])
-    email = StringField('email',[
-        DataRequired(message='Email required'),
-        Length(min=6,max=50)
-        ])
-    password = PasswordField('password',validators=[
-        DataRequired(message='Password required'),
-        Length(min=6,max=12),
-        ])
+    email = StringField('correo', validators=[InputRequired(),
+    Email()
+    ,Length(min=7,max=50,
+    message='El correo debe estar entre 7 y 40 caracteres')])
+
+    password = PasswordField('Contrasena',validators= [InputRequired(), 
+    #EqualTo('password1', 
+    #message='Las contrasenas deben coincidir'),    
+    Length(min=8,max=20,message='Contraseña debe estar entre 8 y 20 caracteres')])
+
+    #password1 = PasswordField('Contrasena1',validators= [     
+    #Length(min=8,max=20,message='Contraseña debe estar entre 8 y 20 caracteres')])
+    
+    captcha = RecaptchaField()
 
 # # FORM RECOVER
 class RecoverForm(Form):
